6e8828b7c11e55bc67ba0d657705536e68936d3c
KienPC-KEN <phungchikien196@gmail.com>
2023-09-23 16:31:06 +0700

    New

---
+++ BE-FoodApp/controllers/product.controller.js
@@ -0,0 +1,3 @@
+exports.getData = (req, res) => {
+    res.send('Hello Cường đẹp trai')
+}
---
+++ BE-FoodApp/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const productRouter = require('./router/product.router')
+
+const app = express();
+
+const uri = "mongodb+srv://phungchikien196:Qa4168ciXnRnjV9G@apppolylib.5gjczzc.mongodb.net/FoodApp"
+mongoose.connect(uri);
+
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use('/image', express.static('image'));
+app.use(bodyParser.json());
+
+app.use('/api', productRouter)
+
+app.get('/', (req, res) => {
+    res.redirect('/api')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+module.exports = app;
