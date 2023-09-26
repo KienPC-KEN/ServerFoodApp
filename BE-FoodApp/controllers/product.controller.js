ce89982a47e9ef9ab7ec6d3ae7cd969836569194
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-09-26 11:14:19 +0700

    update product

--- BE-FoodApp/controllers/product.controller.js
+++ BE-FoodApp/controllers/product.controller.js
@@ -1,4 +1,5 @@
 const product = require('../model/product.model');
+const category = require('../model/category.model');
 
 //[GET]
 const getProduct = async (req, res) => {
@@ -17,12 +18,29 @@ const getProduct = async (req, res) => {
 //[POST]
 const createProduct = async (req, res) => {
       try {
-        if(!req.body) {
-            res.status(404).json('Dữ liệu không được để trống');
-        } else {
-            const newProduct = await product({...req.body}).save();
-            res.status(202).json(newProduct);
-        }
+            if (!req.body) {
+                  res.status(404).json('Dữ liệu không được để trống');
+            } else {
+                  const newProduct = await product({ ...req.body }).save();
+                  res.status(202).json(newProduct);
+            }
+      } catch (error) {
+            res.status(500).json(error);
+      }
+};
+
+//[PUT]
+const updateProduct = async (req, res) => {
+      try {
+            if (!req.body) {
+                  res.status(404).json('Dữ liệu không được để trống');
+            } else {
+                const idCategory = req.body.idCategory;
+                const getCategory = await category.findById({idCategory});
+                if(!getCategory) {
+                    res.status(404).json('Category is not found');
+                }
+            }
       } catch (error) {
             res.status(500).json(error);
       }
