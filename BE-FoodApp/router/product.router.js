ba23c467990455e2652ecda4da0dbc6f65ba15b0
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-09-26 14:47:51 +0700

    search product

--- BE-FoodApp/router/product.router.js
+++ BE-FoodApp/router/product.router.js
@@ -7,5 +7,6 @@ router.get('/', productController.getProduct);
 router.post('/create', productController.createProduct);
 router.put('/update/:id', productController.updateProduct);
 router.delete('/delete/:id', productController.deleteProduct);
+router.get('/search', productController.search);
 
 module.exports = router;
