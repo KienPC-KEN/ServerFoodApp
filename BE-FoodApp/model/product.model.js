7638a6f0a8d675657750863611977ab97ed68a90
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-09-26 10:57:01 +0700

    fix model

--- BE-FoodApp/model/product.model.js
+++ BE-FoodApp/model/product.model.js
@@ -5,7 +5,7 @@ const ProductSchema = new mongoose.Schema({
     image: { type: String, required: true },
     quantity: { type: Number, required: true },
     price: { type: Number, required: true },
-    description: { type: Number, required: true },
+    description: { type: String, required: true },
     idCategory: { type: mongoose.Schema.Types.ObjectId, ref: 'category', required: true },
     idDiscount: { type: mongoose.Schema.Types.ObjectId, ref: 'discount' },
 
