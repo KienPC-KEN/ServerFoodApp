573e165c86e825c53cfbf0fd068dd3b60bf40849
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-09-26 11:14:55 +0700

    update product

--- BE-FoodApp/router/product.router.js
+++ BE-FoodApp/router/product.router.js
@@ -5,5 +5,6 @@ const productController = require("../controllers/product.controller")
 
 router.get('/', productController.getProduct);
 router.post('/create', productController.createProduct);
+router.put('/update/:id', productController.updateProduct);
 
 module.exports = router;
