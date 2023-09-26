f9f83628d5522e94cdfe20873daea640eef9900d
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-09-26 10:43:28 +0700

    get product

--- BE-FoodApp/controllers/product.controller.js
+++ BE-FoodApp/controllers/product.controller.js
@@ -4,10 +4,10 @@ const product = require('../model/product.model');
 const getData = async (req, res) => {
       try {
             const listProduct = await product.find({}).populate('category', 'discount');
-            if (!listProduct) {
-                  res.status(404).json('Data not found');
+            if (listProduct.length == 0) {
+                  res.status(404).json('Data is empty');
             } else {
-                  res.status(202).json(listProduct);
+                  res.status(200).json(listProduct);
             }
       } catch (error) {
             res.status(500).json(error);
