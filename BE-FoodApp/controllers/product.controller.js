14797612f6028a6eeaef51a0282e4b98d81d5b6c
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-09-26 10:35:05 +0700

    Get product

--- BE-FoodApp/controllers/product.controller.js
+++ BE-FoodApp/controllers/product.controller.js
@@ -1,3 +1,19 @@
-exports.getData = (req, res) => {
-    res.send('Hello Cường đẹp trai')
-}
+const product = require('../model/product.model');
+
+//[GET]
+const getData = async (req, res) => {
+      try {
+            const listProduct = await product.find({}).populate('category', 'discount');
+            if (!listProduct) {
+                  res.status(404).json('Data not found');
+            } else {
+                  res.status(202).json(listProduct);
+            }
+      } catch (error) {
+            res.status(500).json(error);
+      }
+};
+
+module.exports = {
+      getData,
+};
