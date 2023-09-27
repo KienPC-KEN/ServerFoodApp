abf90fb2c7b1658d5d523adb3b4efb26099b0d67
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-09-27 07:59:57 +0700

    discount model

---
+++ BE-FoodApp/model/discount.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const discount = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    discountPerson: { type: Number, required: true },
+    dateStart: { type: Date("mm-dd-yyyy"), default: Date.now()},
+    dateEnd: { type: Date("mm-dd-yyyy"), required: true},
+
+});
+
+module.exports = mongoose.model('discount', discount);
