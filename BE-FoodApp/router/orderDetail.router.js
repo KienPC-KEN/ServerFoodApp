3250a4cf2316e025a79d60670d7cab3fe7d942cc
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-09-27 15:58:23 +0700

    order detail router

--- BE-FoodApp/router/orderDetail.router.js
+++ BE-FoodApp/router/orderDetail.router.js
@@ -6,5 +6,6 @@ const orderDetailController = require("../controllers/orderDetail.controller")
 router.get('/', orderDetailController.getData);
 router.post('/create', orderDetailController.createOrderDetail);
 router.put('/update/:id', orderDetailController.updateOrderDetail);
+router.delete('/delete/:id', orderDetailController.deleteOrderDetail);
 
 module.exports = router;
