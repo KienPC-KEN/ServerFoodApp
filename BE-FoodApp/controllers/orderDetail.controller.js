cbac78ad60f1360155f227da00fc12b313484d44
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-09-27 15:51:57 +0700

    order detail controller

--- BE-FoodApp/controllers/orderDetail.controller.js
+++ BE-FoodApp/controllers/orderDetail.controller.js
@@ -3,7 +3,12 @@ const orderDetail = require('../model/orderDetail.model');
 //[GET]
 const getData = async (req, res) => {
       try {
-            
+            const data = await orderDetail.find({}).populate('idOrderItem');
+            if (data.length == 0) {
+                  res.status(404).json('Data is empty');
+            } else {
+                  res.status(200).json(data);
+            }
       } catch (error) {
             res.status(500).json(error);
       }
