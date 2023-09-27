2abb764f6134e69d633108af100ea093d09508e5
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-09-27 08:15:32 +0700

    discount model

--- BE-FoodApp/model/discount.model.js
+++ BE-FoodApp/model/discount.model.js
@@ -4,8 +4,8 @@ const discount = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
     discountPerson: { type: Number, required: true },
-    dateStart: { type: Date("mm-dd-yyyy"), default: Date.now()},
-    dateEnd: { type: Date("mm-dd-yyyy"), required: true},
+    dateStart: { type: Date, default: Date.now()},
+    dateEnd: { type: Date, required: true},
 });
 
 module.exports = mongoose.model('discount', discount);
