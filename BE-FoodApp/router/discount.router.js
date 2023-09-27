140b9996ea96e970b32fd175dfd7a9ee2b832145
cuongvt4510471 <110617055+cuongvt4510471@users.noreply.github.com>
2023-09-27 08:16:52 +0700

    discount router

--- BE-FoodApp/router/discount.router.js
+++ BE-FoodApp/router/discount.router.js
@@ -6,5 +6,6 @@ const discountController = require('../controllers/discount.controller');
 router.get('/', discountController.getDiscount);
 router.post('/create', discountController.createDiscount);
 router.put('/update/:id', discountController.updateDiscount);
+router.delete('/delete/:id', discountController.deleteDiscount);
 
 module.exports = router;
