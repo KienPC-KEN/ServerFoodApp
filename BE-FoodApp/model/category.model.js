2249ee8a516af94beb17aa97db021f2f0b441f6b
KienPC-KEN <phungchikien196@gmail.com>
2023-10-31 16:05:29 +0700

    Update Category

--- BE-FoodApp/model/category.model.js
+++ BE-FoodApp/model/category.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
     name: { type: String, required: true },
+    type: { type: String, required: true },
     createdAt:{type: Date, default: Date.now()}
 
 });
--- BE-FoodApp/router/category.router.js
+++ BE-FoodApp/router/category.router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const categortController = require("../controllers/category.controller")
 
 router.post('/addCategory', categortController.addCategory)
-router.get('/getAllCategory', categortController.getAllCategories)
+router.get('/', categortController.getAllCategories)
 router.put('/updateCategory/:catId', categortController.editCategory)
 router.delete('/deleteCategory/:catId', categortController.deleteCategory)
 
