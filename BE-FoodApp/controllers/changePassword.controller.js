118d7be393c0a920f79a90f2ee08b1ffddb66492
KienPC-KEN <phungchikien196@gmail.com>
2023-11-13 12:04:57 +0700

    Update changePassword.controller.js

--- BE-FoodApp/controllers/changePassword.controller.js
+++ BE-FoodApp/controllers/changePassword.controller.js
@@ -11,33 +11,41 @@ const firebaseConfig = require("../config/firebase.config");
 
 exports.changePassword = async (req, res) => {
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const currentUser = auth.currentUser;
-  const { _id, oldPassword, newPassword } = req.body;
-  const userDocument = await mdUser.findById({ _id: _id });
-  if (currentUser) {
-    const credential = EmailAuthProvider.credential(
-      currentUser.email,
-      oldPassword
-    );
-    if (!credential) {
-      return res.status(400).send("Old password is incorrect.");
-    }
-    if (userDocument.password != oldPassword) {
-        return res.status(400).send("Old password is incorrect.");
-    }
-    reauthenticateWithCredential(currentUser, credential).then(() => {
-      updatePassword(currentUser, newPassword).then(async () => {
-        // Update password in MongoDB
-        
-        userDocument.password = newPassword;
-        await userDocument.save();
+  try {
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    const currentUser = auth.currentUser;
 
-        res.status(200).send(userDocument);
-      });
-    });
-  } else {
-    console.error("User is not logged in.");
+    const { _id, oldPassword, newPassword, reNewPassword } = req.body;
+    const userDocument = await mdUser.findById({ _id: _id });
+    if (currentUser) {
+      // const credential = EmailAuthProvider.credential(
+      //   currentUser.email,
+      //   oldPassword
+      // );
+      // if (!credential) {
+      //   return res.status(400).send("Old password is incorrect.");
+      // }
+      if (userDocument.password != oldPassword) {
+        return res.status(400).send("Old password is incorrect.");
+      }
+      if (reNewPassword != newPassword) {
+        return res.status(400).send("RePassword is incorrect.");
+      }
+      // reauthenticateWithCredential(currentUser, credential).then(() => {
+        updatePassword(currentUser, newPassword).then(async () => {
+          // Update password in MongoDB
+
+          userDocument.password = newPassword;
+          //await userDocument.save();
+
+          res.status(200).send(userDocument);
+        });
+      // });
+    } else {
+      console.error("User is not logged in.");
+    }
+  } catch (error) {
+    console.log(error.message);
   }
 };
